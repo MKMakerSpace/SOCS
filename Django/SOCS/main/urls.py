dff2aee011a777b12f8db8fe50db8af653a64be1
Jack Deanus <33754415+deanusjack@users.noreply.github.com>
2021-05-16 16:04:24 +0100

    Add data hub

--- Django/SOCS/main/urls.py
+++ Django/SOCS/main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='main-home'),
     path('about/', views.about, name="main-about"),
+    path('data/', views.data, name="main-data"),
 ]
--- Django/SOCS/main/views.py
+++ Django/SOCS/main/views.py
@@ -1,6 +1,7 @@
 #Imports
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
@@ -8,3 +9,6 @@ def home(request):
 
 def about(request):
 		return render(request, 'main/about.html')
+@login_required
+def data(request):
+		return render(request, 'main/data.html')
