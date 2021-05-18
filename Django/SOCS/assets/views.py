ff61a097dabfa6b42164cf70157782a0df25b1e6
Jack Deanus <33754415+deanusjack@users.noreply.github.com>
2021-05-18 22:22:36 +0100

    Access Control fix
    Ensure Asset List only viewable by members

--- Django/SOCS/assets/views.py
+++ Django/SOCS/assets/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def asset(request):
 	return render(request, 'assets/list.html')
--- Django/SOCS/main/views.py
+++ Django/SOCS/main/views.py
@@ -1,6 +1,5 @@
 #Imports
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
