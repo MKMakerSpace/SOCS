3c53796a5ddd863a3e994aa6868b9909d95493d7
Jack Deanus <33754415+deanusjack@users.noreply.github.com>
2021-05-21 16:53:34 +0100

    Update Asset Model & Page.

--- Django/SOCS/assets/models.py
+++ Django/SOCS/assets/models.py
@@ -7,20 +7,43 @@ from django.db import models
 
 class Asset(models.Model):
 	#Fields
+	ITEQUIP = 'ITEQUIP'
+	MACHINERY = 'MACHINERY'
+	TOOLS = 'TOOLS'
+	ELECTRICALEQUIP = 'ELECTRICALEQUIP'
+	FURNITURE = 'FURNITURE'
+	DIGITAL = 'DIGITAL'
+	UNCATEGORIZED = 'UNCATEGORIZED'
+
+
+	ASSET_TYPE_CHOICES = [
+		(ITEQUIP,'IT Equipment'),
+		(MACHINERY,'Machinery'),
+		(TOOLS, 'Tools'),
+		(ELECTRICALEQUIP,'Electrical Equipment'),
+		(FURNITURE, 'Furniture'),
+		(DIGITAL, 'Digital'),
+		(UNCATEGORIZED, 'Uncategorized'),
+	]
+
 	AssetID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	AssetType = models.CharField(max_length=50, help_text='Type of Asset',choices=ASSET_TYPE_CHOICES, default = UNCATEGORIZED,)
 	Name = models.CharField(max_length=50, help_text='Item Name')
 	Manufacturer = models.CharField(max_length=50, help_text='Manufacturer Name', blank=True)
 	Model = models.CharField(max_length=50, help_text='Manufacturer Model', blank=True)
-	SerialNo = models.CharField(max_length=50, help_text='Model Serial Number', blank=True)
 	Location = models.CharField(max_length=50, help_text='Location Stored/Kept in space')
 	Owner = models.CharField(max_length=50, help_text='Owner')
 	Description = models.CharField(max_length=200, help_text='Item Description')
-	Notes = models.CharField(max_length=200, help_text='Item Notes', blank=True)
+	Notes = models.CharField(max_length=200, help_text='Item Notes - Include Serial # where applicable.', blank=True)
 	PurchasePrice = models.DecimalField(decimal_places=2, max_digits=9, help_text='Price to purchase/replace the item.')
 	Value = models.DecimalField(decimal_places=2, max_digits=9, help_text='Current value of the item')
 	DateAcquired = models.DateField(help_text='Date item was acquired.')
 	Status = models.CharField(max_length=50, help_text='Current Status of the item.')
 	Warranty = models.DecimalField(decimal_places=3, max_digits=5, help_text='Manufacturer Warranty Period')
+	InspectionPeriod = models.DecimalField(decimal_places=3, max_digits=5, help_text='Period between inspections where applicable in months.',blank=True)
+	NextInspection = models.DateField(help_text='Next inspection date',blank=True)
+	DateRetired = models.DateField(help_text='Date of retirement, destruction or sale etc.', blank=True)
+	
 	#Metadata
 	class Meta:
 		ordering = ['Manufacturer', 'Model']
--- Django/SOCS/assets/views.py
+++ Django/SOCS/assets/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from assets.models import Asset
+from django.db.models import Sum
 
 @login_required
 def asset(request):
 
 	num_assets = Asset.objects.all().count() #Count all objects
-	asset_data = Asset.objects.all() 
+	asset_data = Asset.objects.all()
+	total_value = Asset.objects.aggregate(value=Sum('Value'))['value']
+	total_purchase_value = Asset.objects.aggregate(purchaseprice=Sum('PurchasePrice'))['purchaseprice']
 	title = 'Assets' #Set Page title
 	
 	context = { #Pass Variables into page
-		'title': title,
-		'num_assets': num_assets,
-		'asset_data': asset_data,
+	'title': title,
+	'num_assets': num_assets,
+	'asset_data': asset_data,
+	'total_value': total_value,
+	'total_purchase_value': total_purchase_value
 		
-	}
+		}
 
 	return render(request, 'assets/list.html', context=context)
